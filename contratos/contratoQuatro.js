957251fc83c6abcd1acd2806cc888879799b1b1d
julia-lika <julia.ishikawa@sou.inteli.edu.br>
2024-03-14 20:27:34 -0300

    Upload nas imagens dos contratos

--- contratos/contratoQuatro.js
+++ contratos/contratoQuatro.js
@@ -1,21 +1,39 @@
 class ContratoQuatro extends Phaser.Scene {
-    constructor() {
-      super({ key: "ContratoQuatro" });
-    }
-  
-    preload() {
-      this.load.image("notebook", "assets/notebook.png");
-      this.load.image("pastas", "assets/pasta.png");
-      this.load.image("check", "assets/check.png");
-      this.load.image("deny", "assets/deny.png");
-    }
-  
-    create() {
-      this.add.image(640, 300, "notebook").setScale(0.7);
-      
-    }
-  
-    update() {
-  
-    }
+  constructor() {
+    super({ key: "ContratoQuatro" });
   }
+
+  preload() {
+    this.load.image("notebook", "assets/notebook.png");
+    this.load.image("pastas", "assets/pasta.png");
+    this.load.image("check", "assets/check.png");
+    this.load.image("deny", "assets/deny.png");
+    this.load.image("fornecedor4", "assets/fornecedores/fornecedorSprite_4.png");
+  }
+
+  create() {
+    this.add.image(640, 300, "notebook").setScale(0.7);
+
+    this.add.rectangle(640, 230, 240, 260, "#F5F5DC");
+
+    this.add.image(600, 250, "fornecedor4").setScale(2);
+
+    this.check = this.add.image(675, 410, "check").setScale(6);
+    this.check.setInteractive();
+    this.check.on("pointerdown", () => {
+      this.scene.stop("ContratoQuatro");
+      this.scene.start("Contratos");
+    });
+
+    this.deny = this.add.image(600, 410, "deny").setScale(6);
+    this.deny.setInteractive();
+    this.deny.on("pointerdown", () => {
+      this.scene.stop("ContratoQuatro");
+      this.scene.start("Contratos");
+    });
+
+    this.add.text(400, 200, "Contrato", { fontSize: "30px", fill: "FFFFFF" });
+  }
+
+  update() {}
+}
--- contratos/contratoUm.js
+++ contratos/contratoUm.js
@@ -1,42 +1,39 @@
 class ContratoUm extends Phaser.Scene {
-    constructor() {
-      super({ key: "ContratoUm" });
-    }
-  
-    preload() {
-      this.load.image("notebook", "assets/notebook.png");
-      this.load.image("pastas", "assets/pasta.png");
-      this.load.image("check", "assets/check.png");
-      this.load.image("deny", "assets/deny.png");
-      this.load.image('fornecedor', 'assets/fornecedores/fornecedorSprite_1.png');
-    }
-  
-    create() {
-      this.add.image(640, 300, "notebook").setScale(0.7);
-
-      this.add.rectangle(640, 230, 240, 260, '#F5F5DC');
-
-      this.add.image(600, 250, 'fornecedor').setScale(2);
-
-      this.check = this.add.image(675, 410, "check").setScale(6);
-      this.check.setInteractive();
-      this.check.on ('pointerdown', () => {
-        this.scene.stop('ContratoUm');
-        this.scene.start('Contratos');
-      })
-
-      this.deny = this.add.image(600, 410, "deny").setScale(6);
-      this.deny.setInteractive();
-      this.deny.on ('pointerdown', () => {
-        this.scene.stop('ContratoUm');
-        this.scene.start('Contratos');
-      })
-
-      this.add.text(400, 200, 'Contrato', {fontSize:'30px', fill:'FFFFFF'});
-      
-    }
-  
-    update() {
-  
-    }
+  constructor() {
+    super({ key: "ContratoUm" });
   }
+
+  preload() {
+    this.load.image("notebook", "assets/notebook.png");
+    this.load.image("pastas", "assets/pasta.png");
+    this.load.image("check", "assets/check.png");
+    this.load.image("deny", "assets/deny.png");
+    this.load.image("fornecedor1", "assets/fornecedores/fornecedorSprite_1.png");
+  }
+
+  create() {
+    this.add.image(640, 300, "notebook").setScale(0.7);
+
+    this.add.rectangle(640, 230, 240, 260, "#0081fb");
+
+    this.add.image(600, 250, "fornecedor1").setScale(2);
+
+    this.check = this.add.image(675, 410, "check").setScale(6);
+    this.check.setInteractive();
+    this.check.on("pointerdown", () => {
+      this.scene.stop("ContratoUm");
+      this.scene.start("Contratos");
+    });
+
+    this.deny = this.add.image(600, 410, "deny").setScale(6);
+    this.deny.setInteractive();
+    this.deny.on("pointerdown", () => {
+      this.scene.stop("ContratoUm");
+      this.scene.start("Contratos");
+    });
+
+    this.add.text(400, 200, "Contrato", { fontSize: "30px", fill: "FFFFFF" });
+  }
+
+  update() {}
+}
