90e7fb80ea28020992c08bf410cd1d2c3704b9a9
julia-lika <julia.ishikawa@sou.inteli.edu.br>
2024-03-15 08:10:41 -0300

    estilização dos contratos

--- contratos/contratoUm.js
+++ contratos/contratoUm.js
@@ -8,15 +8,20 @@ class ContratoUm extends Phaser.Scene {
     this.load.image("pastas", "assets/pasta.png");
     this.load.image("check", "assets/check.png");
     this.load.image("deny", "assets/deny.png");
-    this.load.image("fornecedor1", "assets/fornecedores/fornecedorSprite_1.png");
+    this.load.image(
+      "fornecedor1",
+      "assets/fornecedores/fornecedorSprite_1.png"
+    );
   }
 
   create() {
     this.add.image(640, 300, "notebook").setScale(0.7);
 
-    this.add.rectangle(640, 230, 240, 260, "#0081fb");
+    this.add.rectangle(640, 230, 240, 260, 0x000000);
+    const retanguloFoto = this.add.rectangle(570, 210, 50, 50, 0xffffff).setScale(1.5);
+    retanguloFoto.setStrokeStyle(4, 0xefc53f);
 
-    this.add.image(600, 250, "fornecedor1").setScale(2);
+    this.add.image(570, 200, "fornecedor1").setScale(2);
 
     this.check = this.add.image(675, 410, "check").setScale(6);
     this.check.setInteractive();
@@ -32,7 +37,12 @@ class ContratoUm extends Phaser.Scene {
       this.scene.start("Contratos");
     });
 
-    this.add.text(400, 200, "Contrato", { fontSize: "30px", fill: "FFFFFF" });
+    this.add.text(590, 110, "Contrato", { fontSize: "20px", color: "#FFFFFF" });
+  }
+
+  passarDeCena() {
+    this.scene.stop("ContratoUm");
+      this.scene.start("Contratos");
   }
 
   update() {}
